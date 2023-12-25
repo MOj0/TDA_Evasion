ba5504863320726048502004a3a70a2f1194ece5
unknown <matija.ojo@gmail.com>
2023-12-25 20:36:45 +0100

    generalize path

--- evasion.py
+++ evasion.py
@@ -1,4 +1,4 @@
-from typing import TypeAlias, Iterator
+from typing import Iterator
 import math
 import numpy as np
 from collections import defaultdict
@@ -19,56 +19,90 @@ class Position:
         self.x = x
         self.y = y
 
-    def __getitem__(self, item):
-        return self.x if item == 0 else self.y
-
-    def __setitem__(self, key, value):
-        setattr(self, "x" if key == 0 else "y", value)
-
     def __str__(self):
         return f"({self.x}, {self.y})"
 
     def __add__(self, other: "Position") -> "Position":
         return Position(self.x + other.x, self.y + other.y)
 
-    def as_list(self) -> list[int]:
-        return [self.x, self.y]
-    
+    def __sub__(self, other: "Position") -> "Position":
+        return Position(self.x - other.x, self.y - other.y)
+
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
-    
+
     def __hash__(self):
         return hash((self.x, self.y))
 
+    def __len__(self):
+        """returns 0-norm of position"""
+        return abs(self.x) + abs(self.y)
 
-Path: TypeAlias = tuple[Position, Position]
+    def as_list(self) -> list[int]:
+        return [self.x, self.y]
+
+
+class Path:
+    UP = Position(0, -1)
+    RIGHT = Position(1, 0)
+    DOWN = Position(0, 1)
+    LEFT = Position(-1, 0)
+
+    def __init__(self, path: list[Position]):
+        self.path_points = path
+        self.pos = path[0]
+        self.dir_idx = 0
+        self.dirs = [
+            self.get_dir(p2 - p1)
+            for p1, p2 in zip(path, path[1:] + [path[0]])
+            if p1 != p2
+        ]
+
+    def __len__(self) -> int:
+        return sum(
+            len(p2 - p1)
+            for p1, p2 in zip(
+                self.path_points, self.path_points[1:] + [self.path_points[0]]
+            )
+        )
+
+    def next(self) -> Position:
+        self.pos = self.pos + self.dirs[self.dir_idx]
+
+        next_dir_idx = (self.dir_idx + 1) % len(self.dirs)
+        if self.pos == self.path_points[next_dir_idx]:
+            self.dir_idx = next_dir_idx
+
+        return self.pos
+
+    def get_dir(self, pos: Position) -> Position:
+        if pos.x > 0:
+            return Path.RIGHT
+        elif pos.x < 0:
+            return Path.LEFT
+        elif pos.y > 0:
+            return Path.DOWN
+        elif pos.y < 0:
+            return Path.UP
 
 
 class Sensor:
-    def __init__(self, start_at: Position, move_dir: Position, path: Path):
-        self.curr_pos = start_at
-        self.moving_component = 0 if move_dir.x != 0 else 1
-        self.move_dir = move_dir[self.moving_component]
-        self.path_min = min(
-            path[0][self.moving_component],
-            path[1][self.moving_component],
-        )
-        self.path_max = max(
-            path[0][self.moving_component],
-            path[1][self.moving_component],
-        )
+    def __init__(self, path: Path):
+        self.path = path
+
+    def __str__(self):
+        return str(self.curr_pos)
+
+    @property
+    def curr_pos(self) -> Position:
+        return self.path.pos
+
+    @property
+    def period(self) -> int:
+        return len(self.path)
 
     def move(self):
-        next_pos = self.curr_pos[self.moving_component] + self.move_dir
-        if next_pos < self.path_min or next_pos > self.path_max:
-            self.move_dir = -self.move_dir
-
-        self.curr_pos[self.moving_component] += self.move_dir
-
-    def area(self) -> list[Position]:
-        return [
-            self.curr_pos + Position(i, j) for i in range(-1, 2) for j in range(-1, 2)
-        ]
+        self.path.next()
 
     def slice(self) -> gudhi.cubical_complex.CubicalComplex:
         topleft = self.curr_pos + Position(-1, -1)
@@ -79,13 +113,6 @@ class Sensor:
             vertices=[topleft.as_list(), bottomright.as_list()]
         )
 
-    def __str__(self):
-        return str(self.curr_pos)
-    
-    @property
-    def period(self) -> int:
-        return 2 * (self.path_max - self.path_min)
-    
 
 @dataclass
 class SensorNetwork:
@@ -96,10 +123,11 @@ class SensorNetwork:
 
     def __post_init__(self):
         assert all(
-            0 <= s.curr_pos.x < self.room_width and 0 <= s.curr_pos.y < self.room_height
-            for s in self.sensors
-        )
-        # TODO: make sure the paths stay within the room
+            # TODO: Should we allow sensors moving along coordinate 0?
+            0 <= p.x < self.room_width and 0 <= p.y < self.room_height
+            for sensor in self.sensors
+            for p in sensor.path.path_points
+        ), "Path out of bounds"
 
     @functools.cached_property
     def period(self) -> int:
@@ -110,17 +138,17 @@ class SensorNetwork:
         for _ in range(self.period):
             area = []
             for s in self.sensors:
-                area.append(s.slice()) 
+                area.append(s.slice())
                 s.move()
 
-            areas.append(area) # NOTE: could have duplicates (from overlapping sensors)
+            areas.append(area)  # NOTE: could have duplicates (from overlapping sensors)
 
         return areas
-    
+
     def cells_covered_by(self, sensor: Sensor) -> Iterator[Position]:
         # the area covered by the sensors is the union of all the
-        # unit squares that have one of the sensors at one of the vertices. 
-        
+        # unit squares that have one of the sensors at one of the vertices.
+
         # NOTE: sensor Positions are grid vertices, not actual cells:
         # shifted coordinate system applies for cells, where vertex (x,y) is the top-left corner of the cell (x,y)
         # (assuming y axis points down)
@@ -128,7 +156,10 @@ class SensorNetwork:
             for dy in [-1, 0]:
                 adjacent_cell_pos = sensor.curr_pos + Position(dx, dy)
                 # any of these could be out of bounds (if sensor moving along the edge of the room)
-                if 0 <= adjacent_cell_pos.x < self.room_width and 0 <= adjacent_cell_pos.y < self.room_height:
+                if (
+                    0 <= adjacent_cell_pos.x < self.room_width
+                    and 0 <= adjacent_cell_pos.y < self.room_height
+                ):
                     yield adjacent_cell_pos
 
     def covered_slices(self) -> Iterator[set[Position]]:
@@ -143,15 +174,19 @@ class SensorNetwork:
 
     def all_cells(self) -> np.ndarray:
         # NOTE: a sm zamenou height pa width?? (nima veze za sample)
-        return np.array([[[i, j], [i + 1, j + 1]] for i in range(self.room_height) for j in range(self.room_width)])
-    
+        return np.array(
+            [
+                [[i, j], [i + 1, j + 1]]
+                for i in range(self.room_height)
+                for j in range(self.room_width)
+            ]
+        )
 
     @property
     def cell_positions(self) -> Iterator[Position]:
         for x in range(self.room_width):
             for y in range(self.room_height):
                 yield Position(x, y)
-    
 
     def construct_F(self):
         r"""Constructs free subcomplex F = (X * [0, p] \ C)"""
@@ -162,7 +197,9 @@ class SensorNetwork:
 
         for i, (s1, s2) in enumerate(zip(slices, slices[1:] + [slices[0]])):
             for cell in cells:
-                first_cell_free = not any(contains_interval(s.vertices(), cell) for s in s1)
+                first_cell_free = not any(
+                    contains_interval(s.vertices(), cell) for s in s1
+                )
                 second_cell_free = not any(
                     contains_interval(s.vertices(), cell) for s in s2
                 )
@@ -172,7 +209,6 @@ class SensorNetwork:
                     F_complex[(i, i + 1)].append(cell)
 
         return F_complex
-    
 
     def evasion_complex(self) -> CubicalComplex:
         """constructs the complex X * [0, p] where free cubes have filtration value 1 (and 0 otherwise)"""
@@ -190,9 +226,8 @@ class SensorNetwork:
                     # if t <= 1: print(f"free cell {cell} persists {t} -> {t+1}")
                     cube_filtration[cell.x, cell.y, t] = 1
 
-        # NOTE: using a vertices= constructor can lead to an undefined behavior in cofaces_of_persistence_pairs() 
+        # NOTE: using a vertices= constructor can lead to an undefined behavior in cofaces_of_persistence_pairs()
         return CubicalComplex(top_dimensional_cells=cube_filtration)
-
 
     def evasion_paths(self):
         # NOTE: When computing homology, check the generators:
@@ -200,8 +235,11 @@ class SensorNetwork:
         #   cases do not represent a path thief can take
 
         cpx = self.evasion_complex()
-        cube_f = cpx.top_dimensional_cells() # filtration values of the top-dimensional cells (cubes)
-        print(f"{cpx.dimension()}-dim evasion complex with {cube_f.shape}-grid of cubes ({cpx.num_simplices()} simplices)")
+        # filtration values of the top-dimensional cells (cubes)
+        cube_f = cpx.top_dimensional_cells()
+        print(
+            f"{cpx.dimension()}-dim evasion complex with {cube_f.shape}-grid of cubes ({cpx.num_simplices()} simplices)"
+        )
 
         cpx.compute_persistence()
 
@@ -209,7 +247,7 @@ class SensorNetwork:
         # 1st list: numpy arrays of shape (number_of_persistence_points, 2). The indices of the arrays in the list
         # correspond to the homological dimensions, and the integers of each row in each array correspond to:
         # (index of positive top-dimensional cell, index of negative top-dimensional cell).
-        
+
         # The cells are represented by their indices in the input list of top-dimensional cells
         # (and not their indices in the internal datastructure that includes non-maximal cells).
 
@@ -220,11 +258,13 @@ class SensorNetwork:
 
         for dim in range(max(len(persistence_pairs), len(essential_features))):
             print(f"homological dimension {dim}:")
-            print("persistence pairs:") 
+            print("persistence pairs:")
             for pair in persistence_pairs[dim]:
                 birth_idx, death_idx = (np.unravel_index(i, cube_f.shape) for i in pair)
-                print(f"\t{str(birth_idx):<11} [free={cube_f[birth_idx]}] -> {str(death_idx):<11} [free={cube_f[death_idx]}]")
-            
+                print(
+                    f"\t{str(birth_idx):<11} [free={cube_f[birth_idx]}] -> {str(death_idx):<11} [free={cube_f[death_idx]}]"
+                )
+
             if dim < len(essential_features):
                 print("essential features:")
                 for feature in essential_features[dim]:
@@ -234,7 +274,6 @@ class SensorNetwork:
             print()
 
         # could also try the vertices= constructor and then use cpx.vertices_of_persistence_pairs() ...
-    
 
     def animate_coverage(self):
         plt.ioff()
@@ -248,11 +287,12 @@ class SensorNetwork:
             plt.cla()
             plt.imshow(frame)
 
-        ani = matplotlib.animation.FuncAnimation(fig, animate, frames=self.period, interval=1000 * 1, repeat=True)
+        ani = matplotlib.animation.FuncAnimation(
+            fig, animate, frames=self.period, interval=1000 * 1, repeat=True
+        )
         plt.show()
 
 
-1
 def contains_interval(interval: np.ndarray, other: np.ndarray):
     return all(
         element[0] >= boundary[0] and element[1] <= boundary[1]
@@ -264,9 +304,9 @@ def draw3d(cpx: CubicalComplex, free_full=True, alpha=0.5):
     cube_f = cpx.top_dimensional_cells()
 
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(111, projection="3d")
 
-    free_cpx = (cube_f == 1)
+    free_cpx = cube_f == 1
     ax.voxels(free_cpx if free_full else ~free_cpx, alpha=alpha)
 
     ax.set_xlim(0, cube_f.shape[0])
@@ -274,9 +314,9 @@ def draw3d(cpx: CubicalComplex, free_full=True, alpha=0.5):
     ax.set_zlim(0, cube_f.shape[2])
     set_axes_equal(ax)
 
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('time')
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("time")
 
     plt.show()
 
@@ -303,7 +343,7 @@ def set_axes_equal(ax):
 
     # The plot bounding box is a sphere in the sense of the infinity
     # norm, hence I call half the max range the plot radius.
-    plot_radius = 0.5*max([x_range, y_range, z_range])
+    plot_radius = 0.5 * max([x_range, y_range, z_range])
 
     ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
     ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
@@ -311,12 +351,14 @@ def set_axes_equal(ax):
 
 
 SAMPLE_SENSORS = [
-    Sensor(Position(1, 1), Position(0, 1), Path((Position(1, 1), Position(1, 6)))),
-    Sensor(Position(6, 1), Position(1, 0), Path((Position(2, 1), Position(7, 1)))),
-    Sensor(Position(3, 5), Position(0, -1), Path((Position(3, 5), Position(3, 3)))),
-    Sensor(Position(5, 4), Position(0, 1), Path((Position(5, 3), Position(5, 5)))),
-    Sensor(Position(7, 5), Position(0, 1), Path((Position(7, 3), Position(7, 7)))),
-    Sensor(Position(4, 7), Position(-1, 0), Path((Position(1, 7), Position(5, 7)))),
+    Sensor(Path([Position(1, 1), Position(1, 6)])),
+    Sensor(
+        Path([Position(6, 1), Position(7, 1), Position(2, 1)]),
+    ),
+    Sensor(Path([Position(3, 5), Position(3, 3)])),
+    Sensor(Path([Position(5, 4), Position(5, 5), Position(5, 3)])),
+    Sensor(Path([Position(7, 5), Position(7, 7), Position(7, 3)])),
+    Sensor(Path([Position(4, 7), Position(1, 7), Position(5, 7)])),
 ]
 
 sample_network = SensorNetwork(SAMPLE_SENSORS, 8, 8)
